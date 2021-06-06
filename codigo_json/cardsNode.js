c01a3af24a39c1c148fde2607bcd2fd9c1d14bc4
Matheus Jonatha <mthsjonatha@gmail.com>
2021-06-06 20:19:09 -0300

    Initial Overleaf Import

---
+++ codigo_json/cardsNode.js
@@ -0,0 +1,10 @@
+frases = document.querySelectorAll('.td-post-content li');
+frasesArray = [];
+
+for (i = 0; i < frases.length; i++){
+  frasesArray.push(frases[i].innerText);
+}
+
+// console.log(frasesArray); // Usar isso para mostrar o objeto criado.
+
+
